12df01108e37ca07b43544a05466d76681283f85
Ken McDonald <ken@generation.tech>
2017-12-06 21:58:55 -0500

    converted to spaces for indentation

--- lua_environment_dump/env_dump.lua
+++ lua_environment_dump/env_dump.lua
@@ -3,72 +3,72 @@
 env.info('*** LUA _G DUMP START *** ')
 
 basicSerialize = function(s)
-	if s == nil then
-		return "\"\""
-	else
-		if ((type(s) == 'number') or (type(s) == 'boolean') or (type(s) == 'function') or (type(s) == 'table') or (type(s) == 'userdata') ) then
-			return tostring(s)
-		elseif type(s) == 'string' then
-			s = string.format('%q', s)
-			return s
+  if s == nil then
+    return "\"\""
+  else
+    if ((type(s) == 'number') or (type(s) == 'boolean') or (type(s) == 'function') or (type(s) == 'table') or (type(s) == 'userdata') ) then
+      return tostring(s)
+    elseif type(s) == 'string' then
+      s = string.format('%q', s)
+      return s
 --might need to add default else for case where type is novel or undefined
-		end
-	end
+    end
+  end
 end
 
 tableShow = function(tbl, prefix, indent, tableshow_tbls) --based on serialize_slmod, this is a _G serialization
-	tableshow_tbls = tableshow_tbls or {} --create table of tables
-	prefix         = prefix or ""
-	indent         = indent or ""
+  tableshow_tbls = tableshow_tbls or {} --create table of tables
+  prefix         = prefix or ""
+  indent         = indent or ""
 
-	if type(tbl) == 'table' then --function only works for tables!
-		tableshow_tbls[tbl] = prefix
+  if type(tbl) == 'table' then --function only works for tables!
+    tableshow_tbls[tbl] = prefix
 
-		local tbl_str = {}
+    local tbl_str = {}
 
-		tbl_str[#tbl_str + 1] = indent .. '{\n'
+    tbl_str[#tbl_str + 1] = indent .. '{\n'
 
-		for ind,val in pairs(tbl) do -- serialize its fields
-			if type(ind) == "number" then
+    for ind,val in pairs(tbl) do -- serialize its fields
+      if type(ind) == "number" then
 -- is this section redundant with following section because basicSerialize already handles number type in the same manner?
-				tbl_str[#tbl_str + 1] = indent
-				tbl_str[#tbl_str + 1] = prefix .. '['
-				tbl_str[#tbl_str + 1] = tostring(ind)
-				tbl_str[#tbl_str + 1] = '] = '
-			else
-				tbl_str[#tbl_str + 1] = indent
-				tbl_str[#tbl_str + 1] = prefix .. '['
-				tbl_str[#tbl_str + 1] = basicSerialize(ind)
-				tbl_str[#tbl_str + 1] = '] = '
-			end
+        tbl_str[#tbl_str + 1] = indent
+        tbl_str[#tbl_str + 1] = prefix .. '['
+        tbl_str[#tbl_str + 1] = tostring(ind)
+        tbl_str[#tbl_str + 1] = '] = '
+      else
+        tbl_str[#tbl_str + 1] = indent
+        tbl_str[#tbl_str + 1] = prefix .. '['
+        tbl_str[#tbl_str + 1] = basicSerialize(ind)
+        tbl_str[#tbl_str + 1] = '] = '
+      end
 
-			if ((type(val) == 'number') or (type(val) == 'boolean')) then
+      if ((type(val) == 'number') or (type(val) == 'boolean')) then
 --same here. this might also be redundant to next block due to same functionality of basicSerialize
-				tbl_str[#tbl_str + 1] = tostring(val)
-				tbl_str[#tbl_str + 1] = ',\n'
-			elseif type(val) == 'string' then
-				tbl_str[#tbl_str + 1] = basicSerialize(val)
-				tbl_str[#tbl_str + 1] = ',\n'
-			elseif type(val) == 'nil' then -- won't ever happen, right?
-				tbl_str[#tbl_str + 1] = 'nil,\n'
-			elseif type(val) == 'table' then
-				if tableshow_tbls[val] then
+        tbl_str[#tbl_str + 1] = tostring(val)
+        tbl_str[#tbl_str + 1] = ',\n'
+      elseif type(val) == 'string' then
+        tbl_str[#tbl_str + 1] = basicSerialize(val)
+        tbl_str[#tbl_str + 1] = ',\n'
+      elseif type(val) == 'nil' then -- won't ever happen, right?
+        tbl_str[#tbl_str + 1] = 'nil,\n'
+      elseif type(val) == 'table' then
+        if tableshow_tbls[val] then
 --let's add some kind of alert for if this possibility actually occurs
-					tbl_str[#tbl_str + 1] = tostring(val) .. ' already defined: ' .. tableshow_tbls[val] .. ',\n'
-				else
-					tableshow_tbls[val] = prefix ..  '[' .. basicSerialize(ind) .. ']'
-					tbl_str[#tbl_str + 1] = tostring(val) .. ' '
+          tbl_str[#tbl_str + 1] = tostring(val) .. ' already defined: ' .. tableshow_tbls[val] .. ',\n'
+        else
+          tableshow_tbls[val] = prefix ..  '[' .. basicSerialize(ind) .. ']'
+          tbl_str[#tbl_str + 1] = tostring(val) .. ' '
 --recurces here
-					tbl_str[#tbl_str + 1] = tableShow(val,  prefix .. '[' .. basicSerialize(ind).. ']', indent .. '    ', tableshow_tbls)
-					tbl_str[#tbl_str + 1] = ',\n'
-				end
-			elseif type(val) == 'function' then
-				if debug and debug.getinfo then
-					fcnname = tostring(val)
-					local info = debug.getinfo(val, "S")
-					if info.what == "C" then
-						tbl_str[#tbl_str + 1] = string.format('%q', fcnname .. ', C function') .. ',\n'
-					else
+          tbl_str[#tbl_str + 1] = tableShow(val,  prefix .. '[' .. basicSerialize(ind).. ']', indent .. '    ', tableshow_tbls)
+          tbl_str[#tbl_str + 1] = ',\n'
+        end
+      elseif type(val) == 'function' then
+        if debug and debug.getinfo then
+          fcnname = tostring(val)
+          local info = debug.getinfo(val, "S")
+          if info.what == "C" then
+            tbl_str[#tbl_str + 1] = string.format('%q', fcnname .. ', C function') .. ',\n'
+          else
 --						if (string.sub(info.source, 1, 2) == [[./]]) then
 --tbl_str[#tbl_str + 1] = string.format('%q', fcnname .. ', defined in (' .. info.linedefined .. '-' .. info.lastlinedefined .. ')' .. info.source) ..',\n'
               if info.func ~= nil then
@@ -78,24 +78,24 @@ tableShow = function(tbl, prefix, indent, tableshow_tbls) --based on serialize_s
               end
                 tbl_str[#tbl_str + 1] = string.format('%q', fcnname .. ', defined in (' .. info.linedefined .. '-' .. info.lastlinedefined .. ') ' .. info.source .. ' ' .. f) ..',\n'
 --[[
-						else
-							tbl_str[#tbl_str + 1] = string.format('%q', fcnname .. ', defined in (' .. info.linedefined .. '-' .. info.lastlinedefined .. ')') ..',\n'
-						end
+            else
+              tbl_str[#tbl_str + 1] = string.format('%q', fcnname .. ', defined in (' .. info.linedefined .. '-' .. info.lastlinedefined .. ')') ..',\n'
+            end
 ]]--
-					end
+          end
 
-				else
-					tbl_str[#tbl_str + 1] = 'a function,\n'
-				end
-			else
+        else
+          tbl_str[#tbl_str + 1] = 'a function,\n'
+        end
+      else
 --not cool, watch for any of this
-				tbl_str[#tbl_str + 1] = 'unable to serialize value type ' .. basicSerialize(type(val)) .. ' at index ' .. tostring(ind)
-			end
-		end --for ind,val in pairs(tbl) do
+        tbl_str[#tbl_str + 1] = 'unable to serialize value type ' .. basicSerialize(type(val)) .. ' at index ' .. tostring(ind)
+      end
+    end --for ind,val in pairs(tbl) do
 
-		tbl_str[#tbl_str + 1] = indent .. '}'
-		return table.concat(tbl_str)
-	end  --if type(tbl) == 'table' then
+    tbl_str[#tbl_str + 1] = indent .. '}'
+    return table.concat(tbl_str)
+  end  --if type(tbl) == 'table' then
 end
 
 -- turn a simple list into a key/value true table for future lookups
@@ -114,46 +114,46 @@ env.info('*** LUA _G DUMP TABLE SETUP *** ')
 
 --[[
 local systemf = {
-		"_G",						"_ARCHITECTURE", 	"_VERSION",	"assert",		"collectgarbage",
-		"coroutine",		"debug",					"dofile",		"error",		"gcinfo",
-		"getfenv",			"getmetatable",		"ipairs",		"load",			"loadfile",
-		"loadstring",		"log",						"math",			"module",		"newproxy",
-		"next",					"package",				"pairs",		"pcall",		"print",
-		"rawequal",			"rawget",					"rawset",		"select",		"setfenv",
-		"setmetatable",	"string",					"table",		"tonumber",	"tostring",
-		"type",					"unpack",					"xpcall"
-	}
+    "_G",						"_ARCHITECTURE", 	"_VERSION",	"assert",		"collectgarbage",
+    "coroutine",		"debug",					"dofile",		"error",		"gcinfo",
+    "getfenv",			"getmetatable",		"ipairs",		"load",			"loadfile",
+    "loadstring",		"log",						"math",			"module",		"newproxy",
+    "next",					"package",				"pairs",		"pcall",		"print",
+    "rawequal",			"rawget",					"rawset",		"select",		"setfenv",
+    "setmetatable",	"string",					"table",		"tonumber",	"tostring",
+    "type",					"unpack",					"xpcall"
+  }
 
 for k,v in pairs(systemf) do
-	env.info(tostring(k) .. " " .. tostring(v))
+  env.info(tostring(k) .. " " .. tostring(v))
 end
 
 local syst = tableset(systemf)
 ]]--
 
 local syst = tableset({
-		"_G",						"_ARCHITECTURE", 	"_VERSION",	"assert",		"collectgarbage",
-		"coroutine",		"debug",					"dofile",		"error",		"gcinfo",
-		"getfenv",			"getmetatable",		"ipairs",		"load",			"loadfile",
-		"loadstring",		"log",						"math",			"module",		"newproxy",
-		"next",					"package",				"pairs",		"pcall",		"print",
-		"rawequal",			"rawget",					"rawset",		"select",		"setfenv",
-		"setmetatable",	"string",					"table",		"tonumber",	"tostring",
-		"type",					"unpack",					"xpcall"
-	})
+    "_G",						"_ARCHITECTURE", 	"_VERSION",	"assert",		"collectgarbage",
+    "coroutine",		"debug",					"dofile",		"error",		"gcinfo",
+    "getfenv",			"getmetatable",		"ipairs",		"load",			"loadfile",
+    "loadstring",		"log",						"math",			"module",		"newproxy",
+    "next",					"package",				"pairs",		"pcall",		"print",
+    "rawequal",			"rawget",					"rawset",		"select",		"setfenv",
+    "setmetatable",	"string",					"table",		"tonumber",	"tostring",
+    "type",					"unpack",					"xpcall"
+  })
 
 --[[
 for k,v in pairs(syst) do
-	env.info(tostring(k) .. " " .. tostring(v))
+  env.info(tostring(k) .. " " .. tostring(v))
 end
 ]]--
 
 env.info('*** LUA _G DUMP SHOW_G *** ')
 
 for k,v in pairs(_G) do
-	if syst[k] == nil then
-		env.info(tostring(k) .. "[" .. type(k) .. "] :  " .. tostring(v) .. "[" .. type(v) .. "]")
-	end
+  if syst[k] == nil then
+    env.info(tostring(k) .. "[" .. type(k) .. "] :  " .. tostring(v) .. "[" .. type(v) .. "]")
+  end
 end
 
 --[[
